6ec96dfc93d7f64e6c9cbc04256158cd7921b4bd
alxdr <zeng92mail@gmail.com>
2017-03-20 08:58:50 +0000

    add back auth middleware to homecontroller@index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use JavaScript;
 use Faker;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller {
+
+    public function __construct() {
+	$this->middleware('auth');
+    }
 
     public function index(Request $request, $page = 1) {
         JavaScript::put([
