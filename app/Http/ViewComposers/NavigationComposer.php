fcc513baa6fa1617df7e72f9a6e5abeed23ce6d4
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-03-27 03:39:22 +0800

    use database tags for nav bar filter

--- app/Http/ViewComposers/NavigationComposer.php
+++ app/Http/ViewComposers/NavigationComposer.php
@@ -3,13 +3,13 @@
 namespace App\Http\ViewComposers;
 
 use Illuminate\View\View;
+use App\Tag;
 
 class NavigationComposer
 {
     public function compose(View $view)
     {
-        // Hardcoded test data for now
-        $tags = [["id"=> 1, "name"=>"keto", "count"=>30], ["id"=> 2, "name"=>"dining", "count"=>20], ["id"=> 3, "name"=>"low carb", "count"=>15], ["id"=> 4, "name"=>"salad", "count"=>10]];
+        $tags = Tag::all();
         $view->with('tags', $tags);
     }
 }
