20b34ee0c1307171089a154600a372839056005c
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-03-29 13:58:32 +0800

    fix comments
    copy paste fail

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -38,7 +38,7 @@ class CommentController extends Controller {
 		$comments = Comment::with('user')->where('post_id', $postid)->orderBy('created_at', 'desc')->paginate(10);
 		$results = [];
 		foreach ($comments as $comment) {
-			$item = ["text"=>nl2br(e($comment->comment)), "time"=>$post->created_at->diffForHumans(), "id"=>$comment->id,
+			$item = ["text"=>nl2br(e($comment->comment)), "time"=>$comment->created_at->diffForHumans(), "id"=>$comment->id,
 						 "user_id"=>$comment->user_id, "profile_pic"=>$comment->user->profile_pic,
 						 "username"=>$comment->user->username];
 			array_push($results, $item);
