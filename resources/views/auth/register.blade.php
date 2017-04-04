14bb8d6eabb0e2600937f6c425fad18b0a9b7d53
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-04-04 20:23:31 +0800

    update login/register styling

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,9 +3,9 @@
 @section('page-content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+        <div class="container profile-container">
+            <div class="card card-panel">
+                <h2 class="header">Register</h2>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
@@ -62,7 +62,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn waves-effect waves-ligh light-green lighten-1">
                                     Register
                                 </button>
                             </div>
