83c29b1ab7b30fc3817e7063ba18f7d5535b5842
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-04-05 14:33:52 +0800

    styling in password reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -3,8 +3,8 @@
 @section('page-content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+        <div class="container profile-container">
+            <div class="card card-panel">
                 <div class="panel-heading">Reset Password</div>
 
                 <div class="panel-body">
