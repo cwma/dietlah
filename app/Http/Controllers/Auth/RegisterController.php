4dd766d869d9b45bfd23b434382220af5024b76f
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-04-06 19:59:11 +0800

    change username length to 3-20 too long can overflow our ui

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'username' => 'required|min:6|max:255|unique:users',
+            'username' => 'required|min:3|max:20|unique:users',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
