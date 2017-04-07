4878a28b8dfb546583484a7b344592a4c1a39fdd
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-04-07 15:28:18 +0800

    update maintenance template

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,6 +1,6 @@
 @extends('template')
 
-@section('title', 'Unauthorized')
+@section('title', 'Down for maintenance')
 
 @section('page-content')
 <div class="container">
@@ -8,7 +8,9 @@
     <h1 class="header center light-green-text"><img src="logo.png"></h1>
     <h1 class="header center light-green-text">Be right back!</h1>
     <div class="row center">
-        <h5 class="header col s12 light">We're down for maintenance for some quick changes.<a href="https://docs.google.com/forms/d/e/1FAIpQLScPkJL7MgFZJVzwli_D0fqGhar65LhotL2mLfAbLdbt7oL5dQ/viewform?usp=sf_link" target="_blank">drop us a message.</a></h5>
+        <h5 class="header col s12 light">We're down for maintenance for some quick changes.</h5>
+        <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <a href="https://twitter.com/DietLahSG" class="twitter-follow-button" data-show-count="false">Follow @DietLahSG</a>
     </div>
 </div>
 @stop
