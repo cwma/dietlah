b979f2af902fd6c9a11f8611c7824e0d58873785
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-04-07 15:24:49 +0800

    custom maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,14 @@
+@extends('template')
+
+@section('title', 'Unauthorized')
+
+@section('page-content')
+<div class="container">
+    <br><br>
+    <h1 class="header center light-green-text"><img src="logo.png"></h1>
+    <h1 class="header center light-green-text">Be right back!</h1>
+    <div class="row center">
+        <h5 class="header col s12 light">We're down for maintenance for some quick changes.<a href="https://docs.google.com/forms/d/e/1FAIpQLScPkJL7MgFZJVzwli_D0fqGhar65LhotL2mLfAbLdbt7oL5dQ/viewform?usp=sf_link" target="_blank">drop us a message.</a></h5>
+    </div>
+</div>
+@stop
