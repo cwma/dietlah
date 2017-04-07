0fef67530bec643d58f22df5e78249ec49adc50b
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-04-07 16:14:09 +0800

    update favicon for all browsers

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,13 +5,18 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#ffffff">
         @yield('meta')
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1d9f3c">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyloadxt/1.0.0/jquery.lazyloadxt.fadein.css">
         <link rel="stylesheet" href="css/materialize-tags.min.css">
         <link rel="stylesheet" href="css/dietlah.css">
-        <link rel="icon" href="favicon.ico" type="image/x-icon">
         @yield('stylesheets')
         <title>@yield('title')</title>
     </head>
